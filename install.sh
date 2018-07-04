24bbd839b4c612aac217991d5736d6d7637f68bc
Pieter-Jan Smets <smets.pj@gmail.com>
2018-07-04 19:16:40 +0200

    Move variable creation to function
    Until now, variables were created with a big if-statement
    that you had to copy for every new variable. Now there's
    a function that you can call. It is used as follows:
    
      read_variable <variable_name> <description>
    
    The function checks if the variable is already set. If that
    is the case, the user is asked if he wants to change the
    value. In case it doesn't exist yet, the user is prompted
    for the value.
    
    The description is used in the prompts, and also as a
    comment in user_config.sh above the line where the variable
    is declared.

--- install.sh
+++ install.sh
@@ -9,8 +9,41 @@
 # Generate user_config.sh
 ############################################################
 
+# Let the user enter the value of a variable that should
+# be set in user_config.sh. The first argument to this
+# function is the variable name, the second the description
+# of what is inside the variable.
+read_variable() {
+    local varname description
+    varname=$1
+    description=$2
+
+    echo ""
+
+    if [[ ${!varname} ]]
+    then
+        echo "$description is already set."
+        echo "$varname=${!varname}"
+        read -p "Do you wish to overwrite? [y/N] " INPUT
+    
+    	if [[ "$INPUT" == "y" ]]
+    	then
+            read -p "Enter $description: " INPUT 
+            echo "# $description" >> user_config.sh
+            echo "export $varname=\"$INPUT\"" >> user_config.sh
+    	else
+            echo "keeping old value"
+            echo "# $description" >> user_config.sh
+            echo "export $varname=\"${!varname}\"" >> user_config.sh
+    	fi
+    else
+    	read -p "Enter $description: " INPUT 
+    	echo "export $varname=\"$INPUT\"" >> user_config.sh
+    fi
+}
+
 echo "#" > user_config.sh
-echo "# Autocomplete.sh" >> user_config.sh
+echo "# user_config.sh" >> user_config.sh
 echo "#" >> user_config.sh
 echo "# This is a generated file. It contains the user_config that" >> user_config.sh
 echo "# were set by the user at install time." >> user_config.sh
@@ -18,33 +51,17 @@ echo "#" >> user_config.sh
 echo "" >> user_config.sh
 
 
-if [[ $PROJECTS_FOLDER ]]
-then
-	echo "Projects folder is already set."
-	read -p "Do you wish to overwrite? [y/N] " INPUT
-
-	if [[ "$INPUT" == "y" ]]
-	then
-		read -p "Enter the path to the projects root folder: " INPUT 
-		echo "export PROJECTS_FOLDER=\"$INPUT\"" >> user_config.sh
-	else
-		echo "keeping old value"
-		echo "export PROJECTS_FOLDER=\"$PROJECTS_FOLDER\"" >> user_config.sh
-	fi
-else
-	read -p "Enter the path to the projects root folder: " INPUT 
-	echo "export PROJECTS_FOLDER=\"$INPUT\"" >> user_config.sh
-fi
+read_variable PROJECTS_FOLDER "your projects root folder"
 
 
 ############################################################
 # Modify .bashrc
 ############################################################
 script_dir_path=$(dirname $(readlink -f $0))
-# local script_dir_name=$(basename $SCRIPT_DIR_PATH)
 
 if [[ $SCRIPT_DIR_PATH ]]
 then
+    echo ""
 	echo ".bashrc is already configured."
 else
 	echo "" >> ~/.bashrc
