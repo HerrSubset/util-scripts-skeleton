b87831c5eb4d370d8bbee69a9192877c724b81f9
Pieter-Jan Smets <smets.pj@gmail.com>
2019-06-19 18:23:15 +0200

    Do some work on the builtin functions
    You no longer have to provide an error message when
    asserting a config value, the message is now within the
    function itself. Also add a utility method to print a
    warning, and one to print an error. The latter also exits
    the shell.

--- builtins.sh
+++ builtins.sh
@@ -7,29 +7,34 @@
 # Check if a binary is on the path. If not, exit the 
 # current process.
 # argument 1: the binary that should be present
-# argument 2: an optional error message
 assert_installed() {
 	local program_name=$1
-	local message=$2
 
 	if [[ ! $(which $program_name 2> /dev/null ) ]]
 	then
-		echo "$message"
-		exit 1	
+		die "This command requires '$program_name' to be installed and on the PATH"
 	fi
 }
 
-# Check if an environment variable is set. If not, exit the
-# current process.
-# argument 1: the variable that has to be set
-# argument 2: an optional error message
-assert_value_set() {
+# Check if a variable in user_config.sh is set. If not,
+# exit and tell the user to set the variable by running
+# scripts-reconfigure.
+assert_configured() {
 	local varname=$1
-	local message=$2
 
-	if [[ -z ${!varname} ]]
-	then
-		echo "$message"
-		exit 1
+	if [[ -z ${!varname} ]]; then
+		die "$varname is not set. Please run 'scripts-reconfigure' and set it."
 	fi
 }
+
+# Print an error message in red and exit the script.
+die () {
+	echo -e "\033[0;31m[ERROR] $1\033[0m"
+	exit 1
+}
+
+# Print a warning message in brown/orange
+warn() {
+	echo -e "\033[0;33m[WARNING] $1\033[0m"
+}
+
